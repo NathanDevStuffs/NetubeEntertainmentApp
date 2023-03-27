997ad2021f86884c187e84261b920de3de1736bc
Nathan <79060151+NathanDevStuffs@users.noreply.github.com>
2023-03-27 18:03:32 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
+const path = require('path');
+
+// Set the update server URL
+autoUpdater.setFeedURL({
+  "provider": "github",
+  "owner": "NathanDevStuffs",
+  "repo": "NetubeEntertainmentApp",
+  "releaseType": "release"
+});
+
+app.on("ready", () => {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 1000,
+    autoHideMenuBar: true,
+  })
+
+  win.loadFile('index.html');
+
+  // Check for updates on app startup
+  autoUpdater.checkForUpdatesAndNotify();
+});
